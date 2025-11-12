0988f2f4ba191d14f165a7e04f61058f3bbe1368
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2025-11-12 18:21:44 -0500

    Performance

--- mk_descriptions.py
+++ mk_descriptions.py
@@ -3,6 +3,8 @@
 
 import psycopg
 import mk_requirement_dicts
+import sys
+
 from bisect import bisect
 from collections import defaultdict, namedtuple
 from psycopg.rows import namedtuple_row
@@ -10,30 +12,34 @@ from psycopg.rows import namedtuple_row
 SC = namedtuple('SC', 'course_id offer_nbr min_gpa req_info')
 DC = namedtuple('DC', 'course_id offer_nbr is_pseudo req_info')
 
+error_log = open('./description_errors.log', 'w')
+
 mk_requirement_dicts.mk_dicts()
 course_info = defaultdict(dict)
-conn = psycopg.connect("dbname=cuny_curriculum")
-with conn.cursor(row_factory=namedtuple_row) as cursor:
-  # Cache course info dicts
-  cursor.execute("""
-  select institution, course_id, offer_nbr,
-       discipline||' '||catalog_number as course,
-       min_credits, max_credits,
-       designation in ('MLA', 'MNL') as is_mesg, course_status, career,
-       attributes, requirements
-    from cuny_courses
-  """)
-  for row in cursor.fetchall():
-    is_pseudo = row.is_mesg or 'BKCR' in row.attributes
-    credits = row.max_credits if row.min_credits == row.max_credits else 'varies'
-    requirements = row.requirements
-    course_info[row.course_id][row.offer_nbr] = {'institution': row.institution,
-                                                 'course': row.course,
-                                                 'credits': credits,
-                                                 'is_pseudo': is_pseudo,
-                                                 'status': row.course_status,
-                                                 'career': row.career,
-                                                 'requirements': requirements}
+with psycopg.connect("dbname=cuny_curriculum") as conn:
+  with conn.cursor(row_factory=namedtuple_row) as cursor:
+    # Cache course info dicts
+    cursor.execute("""
+    select institution, course_id, offer_nbr,
+        discipline||' '||catalog_number as course,
+        min_credits, max_credits,
+        designation in ('MLA', 'MNL') as is_mesg,
+        attributes ~* 'bkcr' as is_bkcr,
+        course_status, career,
+        requirements
+      from cuny_courses
+    """)
+    for row in cursor.fetchall():
+      credits = row.max_credits if row.min_credits == row.max_credits else 'varies'
+      requirements = row.requirements
+      course_info[row.course_id][row.offer_nbr] = {'institution': row.institution,
+                                                   'course': row.course,
+                                                   'credits': credits,
+                                                   'is_mesg': row.is_mesg,
+                                                   'is_bkcr': row.is_bkcr,
+                                                   'status': row.course_status,
+                                                   'career': row.career,
+                                                   'requirements': requirements}
 
 
 # min_grade()
@@ -83,49 +89,50 @@ def get_rule_info(rule_key: str) -> namedtuple:
 
       Uses current information about courses, which might be inappropriate.
   """
-  with conn.cursor(row_factory=namedtuple_row) as cursor:
-    cursor.execute("""
-    SELECT
-      r.rule_key,
-      COALESCE(s.source_courses, '[]'::jsonb)      AS source_courses,
-      COALESCE(d.destination_courses, '[]'::jsonb) AS destination_courses
-    FROM transfer_rules AS r
-    LEFT JOIN LATERAL (
-      SELECT jsonb_agg(sc_obj ORDER BY sc_obj->>'course_id',
-                                       sc_obj->>'offer_nbr') AS source_courses
-      FROM (
-        SELECT DISTINCT jsonb_build_object(
-          'course_id', sc.course_id,
-          'offer_nbr', sc.offer_nbr,
-          'min_gpa',   sc.min_gpa
-        ) AS sc_obj
-        FROM source_courses AS sc
-        WHERE sc.rule_id = r.id
-      ) AS x
-    ) AS s ON TRUE
-    LEFT JOIN LATERAL (
-      SELECT jsonb_agg(dc_obj ORDER BY dc_obj->>'course_id',
-                                       dc_obj->>'offer_nbr') AS destination_courses
-      FROM (
-        SELECT DISTINCT jsonb_build_object(
-          'course_id', dc.course_id,
-          'offer_nbr', dc.offer_nbr,
-          'is_mesg',   dc.is_mesg,
-          'is_bkcr',   dc.is_bkcr
-        ) AS dc_obj
-        FROM destination_courses AS dc
-        WHERE dc.rule_id = r.id
-      ) AS y
-    ) AS d ON TRUE
-    WHERE r.rule_key = %s     -- or = $1
-    """, (rule_key,))
-    match cursor.rowcount:
-      case 0:
-        return None
-      case 1:
-        return cursor.fetchone()
-      case _:
-        raise ValueError(f'Multiple instances of {rule_key}')
+  with psycopg.connect('dbname=cuny_curriculum') as conn:
+    with conn.cursor(row_factory=namedtuple_row) as cursor:
+      cursor.execute("""
+      SELECT
+        r.rule_key,
+        COALESCE(s.source_courses, '[]'::jsonb)      AS source_courses,
+        COALESCE(d.destination_courses, '[]'::jsonb) AS destination_courses
+      FROM transfer_rules AS r
+      LEFT JOIN LATERAL (
+        SELECT jsonb_agg(sc_obj ORDER BY sc_obj->>'course_id',
+                                        sc_obj->>'offer_nbr') AS source_courses
+        FROM (
+          SELECT DISTINCT jsonb_build_object(
+            'course_id', sc.course_id,
+            'offer_nbr', sc.offer_nbr,
+            'min_gpa',   sc.min_gpa
+          ) AS sc_obj
+          FROM source_courses AS sc
+          WHERE sc.rule_id = r.id
+        ) AS x
+      ) AS s ON TRUE
+      LEFT JOIN LATERAL (
+        SELECT jsonb_agg(dc_obj ORDER BY dc_obj->>'course_id',
+                                        dc_obj->>'offer_nbr') AS destination_courses
+        FROM (
+          SELECT DISTINCT jsonb_build_object(
+            'course_id', dc.course_id,
+            'offer_nbr', dc.offer_nbr,
+            'is_mesg',   dc.is_mesg,
+            'is_bkcr',   dc.is_bkcr
+          ) AS dc_obj
+          FROM destination_courses AS dc
+          WHERE dc.rule_id = r.id
+        ) AS y
+      ) AS d ON TRUE
+      WHERE r.rule_key = %s     -- or = $1
+      """, (rule_key,))
+      match cursor.rowcount:
+        case 0:
+          return None
+        case 1:
+          return cursor.fetchone()
+        case _:
+          raise ValueError(f'Multiple instances of {rule_key}')
 
 
 # describe_rule()
@@ -133,7 +140,6 @@ def get_rule_info(rule_key: str) -> namedtuple:
 def describe_rule(row: namedtuple) -> tuple:
   if not row:
     return 'No Rule Information'
-  rule_key = row.rule_key
   src_list = []
   dst_list = []
   # Gather information for all source courses
@@ -162,6 +168,7 @@ def describe_rule(row: namedtuple) -> tuple:
           this_course['aliases'].append(src_info['course'])
 
       if this_course['offer_nbr'] is None:
+        this_course['course'] = 'Not a course'
         raise KeyError('offer_nbr not in src_infos')
 
       aliases = (f' (={','.join(this_course['aliases'])})' if this_course['aliases']
@@ -169,17 +176,15 @@ def describe_rule(row: namedtuple) -> tuple:
       src_list.append(f'{this_course['course']}{aliases} '
                       f'{this_course['min_grade']} ' f'[{this_course['requirements']}]')
   except KeyError as err:
-    print(f'src: ‘{err}’ {row.rule_key:20} {course_id:06}:{offer_nbr} {min_gpa:6} '
-          f'{src_infos}')
+    print(f'src: {err} {row.rule_key:20} {course_id:06}:{offer_nbr} {min_gpa:6} '
+          f'{src_infos}', file=error_log)
 
   # Gather the information for all destination courses
   destination_courses = [(destination_course['course_id'],
-                          destination_course['offer_nbr'],
-                          destination_course['is_mesg'],
-                          destination_course['is_bkcr'])
+                          destination_course['offer_nbr'])
                          for destination_course in row.destination_courses]
   try:
-    for course_id, offer_nbr, is_mesg, is_bkcr in destination_courses:
+    for course_id, offer_nbr in destination_courses:
       this_course = {'course_id': None,
                      'offer_nbr': None,
                      'course': '',
@@ -195,13 +200,14 @@ def describe_rule(row: namedtuple) -> tuple:
           this_course['course_id'] = course_id
           this_course['offer_nbr'] = offer_nbr
           this_course['course'] = dst_info['course']
-          this_course['mesg'] = 'M' if is_mesg else '-'
-          this_course['bkcr'] = 'B' if is_bkcr else '-'
+          this_course['mesg'] = 'M' if dst_info['is_mesg'] else '-'
+          this_course['bkcr'] = 'B' if dst_info['is_bkcr'] else '-'
           this_course['requirements'] = format_requirements(dst_info['requirements'])
         else:
           this_course['aliases'].append(dst_info['course'])
 
       if this_course['offer_nbr'] is None:
+        this_course['course'] = 'Not a course'
         raise KeyError('offer_nbr not in dst_infos')
       aliases = (f' (={','.join(this_course['aliases'])})' if this_course['aliases']
                  else '')
@@ -209,85 +215,119 @@ def describe_rule(row: namedtuple) -> tuple:
                       f'{this_course['mesg']}{this_course['bkcr']} '
                       f'[{this_course['requirements']}]')
   except KeyError as err:
-    print(f'dst: ‘{err}’ {row.rule_key:20} {course_id:06}:{offer_nbr} {dst_infos}')
+    print(f'dst: {err} {row.rule_key:20} {course_id:06}:{offer_nbr} {dst_infos}', file=error_log)
 
-  return (rule_key, f'{oxfordize(src_list)} => {oxfordize(dst_list)}')
+  return (row.rule_key, row.effective_date, f'{oxfordize(src_list)} => {oxfordize(dst_list)}')
+
+
+# describe_rules()
+# -------------------------------------------------------------------------------------------------
+def describe_rules(schema_name: str) -> list:
+  """Describe all the rules in a schema."""
+  all_descriptions = []
+  with psycopg.connect('dbname=cuny_curriculum') as conn:
+    with conn.cursor(row_factory=namedtuple_row) as cursor:
+      # Make sure the schema exists
+      cursor.execute("""
+      select schema_name
+        from information_schema.schemata
+      where schema_name = %s""", (schema_name, ))
+      if cursor.rowcount == 0:
+        raise ValueError(f'schema {schema_name} does not exist')
+
+      # Create views to handle difference between public and temp schemata
+      if schema_name == 'public':
+        cursor.execute("""
+        create or replace view public.source_courses_u as
+          select tr.rule_key,
+                sc.course_id, sc.offer_nbr, sc.max_credits, sc.min_gpa
+          from   public.source_courses sc
+          join   public.transfer_rules tr on tr.id = sc.rule_id;
+
+        create or replace view public.destination_courses_u as
+          select tr.rule_key,
+                dc.course_id, dc.offer_nbr
+          from   public.destination_courses dc
+          join   public.transfer_rules tr on tr.id = dc.rule_id;
+        """)
+      else:
+        cursor.execute(f"""
+        create or replace view {schema_name}.source_courses_u as
+          select rule_key, course_id, offer_nbr, max_credits, min_gpa
+          from   {schema_name}.source_courses;
+
+        create or replace view {schema_name}.destination_courses_u as
+          select rule_key, course_id, offer_nbr
+          from   {schema_name}.destination_courses;
+        """)
+
+      # Gather the information needed to describe the rules
+      cursor.execute(f"""
+      WITH
+      sc AS (
+        SELECT rule_key,
+              jsonb_agg(
+                jsonb_build_object(
+                  'course_id', course_id,
+                  'offer_nbr', offer_nbr,
+                  'min_gpa',   min_gpa,
+                  'max_credits', max_credits
+                )
+                ORDER BY course_id, offer_nbr, coalesce(min_gpa, 0.0), coalesce(max_credits, 99.0)
+              ) AS source_courses
+        FROM {schema_name}.source_courses_u
+        GROUP BY rule_key
+      ),
+      dc AS (
+        SELECT rule_key,
+              jsonb_agg(
+                jsonb_build_object(
+                  'course_id', course_id,
+                  'offer_nbr', offer_nbr
+                )
+                ORDER BY course_id, offer_nbr
+              ) AS destination_courses
+        FROM {schema_name}.destination_courses_u
+        GROUP BY rule_key
+      )
+      SELECT
+        r.rule_key,
+        r.effective_date,
+        coalesce(sc.source_courses, '[]'::jsonb) AS source_courses,
+        coalesce(dc.destination_courses, '[]'::jsonb) AS destination_courses
+      FROM {schema_name}.transfer_rules r
+      LEFT JOIN sc USING (rule_key)
+      LEFT JOIN dc USING (rule_key)
+      ORDER BY r.rule_key;
+
+      """)
+      for row in cursor.fetchall():
+        all_descriptions.append(describe_rule(row))
+
+  return all_descriptions
 
 
 # __main__():
 # -------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
 
-  with conn.cursor(row_factory=namedtuple_row) as cursor:
+  schema_name = sys.argv[1] if len(sys.argv) > 1 else 'public'
 
-    # Cache course info dicts
-    cursor.execute("""
-    select institution, course_id, offer_nbr,
-         discipline||' '||catalog_number as course,
-         min_credits, max_credits,
-         designation in ('MLA', 'MNL') as is_mesg, course_status, career,
-         attributes, requirements
-      from cuny_courses
-    """)
-    course_info = defaultdict(dict)
-    for row in cursor.fetchall():
-      is_pseudo = row.is_mesg or 'BKCR' in row.attributes
-      credits = row.max_credits if row.min_credits == row.max_credits else 'varies'
-      requirements = row.requirements
-      course_info[row.course_id][row.offer_nbr] = {'institution': row.institution,
-                                                   'course': row.course,
-                                                   'credits': credits,
-                                                   'is_pseudo': is_pseudo,
-                                                   'status': row.course_status,
-                                                   'career': row.career,
-                                                   'requirements': requirements}
+  all_descriptions = describe_rules(schema_name)
+  with conn.connect('dbname=cuny_curriculum') as conn:
+    with conn.cursor() as cursor:
+      # (Re)create the rule_descriptions table
+      cursor.execute(f"""
+        drop table if exists {schema_name}.rule_descriptions;
+        create table {schema_name}.rule_descriptions (
+          rule_key       text primary key,
+          effective_date text,
+          description    text
+        )
+        """)
+      with cursor.copy(f'copy {schema_name}.rule_descriptions '
+                       f'(rule_key, effective_date, description) from stdin') as cpy:
+        for row in all_descriptions:
+          cpy.write_row(row)
 
-    # Generate all descriptions
-    all_descriptions = []
-    cursor.execute("""
-    SELECT
-      rule_key,
-      COALESCE(s.source_courses, '[]'::jsonb)    AS source_courses,
-      COALESCE(d.destination_courses, '[]'::jsonb) AS destination_courses
-    FROM transfer_rules AS r
-
-    LEFT JOIN LATERAL (
-      SELECT jsonb_agg(sc_obj ORDER BY sc_obj->>'course_id', sc_obj->>'offer_nbr')
-         AS source_courses
-      FROM (
-      SELECT DISTINCT jsonb_build_object(
-        'course_id',   sc.course_id,
-        'offer_nbr',   sc.offer_nbr,
-        'min_gpa',   sc.min_gpa
-      ) AS sc_obj
-      FROM source_courses AS sc
-      WHERE sc.rule_id = r.id
-      ) AS x
-    ) AS s ON TRUE
-
-    LEFT JOIN LATERAL (
-      SELECT jsonb_agg(dc_obj ORDER BY dc_obj->>'course_id', dc_obj->>'offer_nbr')
-         AS destination_courses
-      FROM (
-      SELECT DISTINCT jsonb_build_object(
-        'course_id', dc.course_id,
-        'offer_nbr', dc.offer_nbr,
-        'is_mesg',   dc.is_mesg,
-        'is_bkcr',   dc.is_bkcr
-      ) AS dc_obj
-      FROM destination_courses AS dc
-      WHERE dc.rule_id = r.id
-      ) AS y
-    ) AS d ON TRUE
-
-    ORDER BY r.rule_key
-    """)
-    for row in cursor.fetchall():
-      all_descriptions.append(describe_rule(row))
-
-    # Replace all rows in the rule_descriptions table
-    cursor.execute('truncate rule_descriptions')
-    with cursor.copy('copy rule_descriptions (rule_key, description) from stdin') as cpy:
-      for row in all_descriptions:
-        cpy.write_row(row)
-    print(f'Generted {len(all_descriptions):,} rule_descriptions')
+  print(f'Generated {len(all_descriptions):,} rule_descriptions in schema {schema_name}')
